8f4c7995c17dcade8ded18c9b492488d4d8d801f
Jens Kauteren <koekskeinc@gmail.com>
2026-04-20 16:32:55 +0200

    detox

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776690887|327510690';
+const CACHE_VERSION = '1776695567|1232313126';
 /** @type {string} */
 const CACHE_PREFIX = 'Aha Web-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
